06b5b59385162fc6e907857265904616c1408101
amblafont <chaster_killer@hotmail.fr>
2026-09-16 10:47:49 +0200

    make vite reload when default_sorts.ts is updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { execSync } from 'child_process'
+
+function buildSortsPlugin() {
+  return {
+    name: 'build-sorts',
+    configureServer(server) {
+      server.watcher.add('src/default_sorts.ts')
+      server.watcher.on('change', (file) => {
+        if (file.endsWith('default_sorts.ts')) {
+          console.log('[build:sorts] Regenerating default_sorts.js...')
+          execSync('npm run build:sorts')
+          console.log('[build:sorts] Done.')
+        }
+      })
+    }
+  }
+}
 
 export default defineConfig({
   base: './',
-  plugins: [svelte()]
+  plugins: [svelte(), buildSortsPlugin()]
 })
